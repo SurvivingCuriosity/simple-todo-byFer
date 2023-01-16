651c2ce894a0a29b1a132d0ed8592d6c5a199ba2
FXY <ferrodest1999@gmail.com>
2023-01-16 15:07:34 +0100

    Added README

--- src/components/PantallaAjustes.js
+++ src/components/PantallaAjustes.js
@@ -4,20 +4,20 @@ import icono_anadir_enabled from '../imgs/icono_anadir_enabled.svg'
 import icono_borrar_rojo from '../imgs/icono_borrar_rojo.svg'
 import { BotonClaroOscuro } from './BotonClaroOscuro'
 
-export function PantallaAjustes(props){
-    const {tema, activarTema, callbackNuevaCategoria, handleCloseCallback, handleBorrarCategoria} = props;
+export function PantallaAjustes(props) {
+    const { tema, activarTema, callbackNuevaCategoria, handleCloseCallback, handleBorrarCategoria } = props;
     const [tituloCategoria, setTituloCategoria] = React.useState('');
     const [mostrandoFormNuevaCategoria, setMostrandoFormNuevaCategoria] = React.useState(false);
-    
-    React.useEffect(()=>{
-        window.addEventListener('mousedown',handleClickScreen)
-        function handleClickScreen(evt){
-            if(evt.target.nodeName==='DIV' || !evt.target.className==='input-crear-cat'){
+
+    React.useEffect(() => {
+        window.addEventListener('mousedown', handleClickScreen)
+        function handleClickScreen(evt) {
+            if (evt.target.nodeName === 'DIV' || !evt.target.className === 'input-crear-cat') {
                 handleCloseCallback();
             }
         }
         return () => window.removeEventListener("mousedown", handleClickScreen);
-    },[])
+    }, [])
 
     const handleChange = (evt) => {
         setTituloCategoria(evt.target.value)
@@ -31,66 +31,67 @@ export function PantallaAjustes(props){
     const borrarCategoria = (evt) => {
         handleBorrarCategoria(evt.target.name);
     }
-    return(
+    return (
         <div className='pantalla-ajustes fade-in'>
+            <div className='container-pantalla-ajustes'>
+                <BotonClaroOscuro
+                    temaActivo={tema}
+                    callback={activarTema}
+                />
 
-            <BotonClaroOscuro
-				temaActivo={tema}
-				callback={activarTema}
-			/>
-            
-            <h2>CATEGORÍAS</h2>
+                <h2>CATEGORÍAS</h2>
                 <ul>
-                    {props.categorias.length > 0 
-                    ?
-                        props.categorias.map((cat)=>{
-                            if(cat==='Todas'){
+                    {props.categorias.length > 0
+                        ?
+                        props.categorias.map((cat) => {
+                            if (cat === 'Todas') {
                                 return
-                            }else{
-                                return(
-                                    <li key={cat+Date.now()} className='input-and-button'>
+                            } else {
+                                return (
+                                    <li key={cat + Date.now()} className='input-and-button'>
                                         <p>{cat}</p>
                                         <img src={icono_borrar_rojo} onClick={borrarCategoria} name={cat}></img>
                                     </li>
                                 )
                             }
                         })
-                    :
-                        <p style={{fontSize:'0.7em'}}>No has creado ninguna categoría</p>
+                        :
+                        <p style={{ fontSize: '0.7em' }}>No has creado ninguna categoría</p>
                     }
                 </ul>
 
-            
-            {
-                mostrandoFormNuevaCategoria===true
-                ? 
-                <>
-                    <form onSubmit={handleSubmit}>
-                        <div className="input-and-button">
-                            <input
-                                className='input-text-crear-cat'
-                                onChange={handleChange}
-                                value={tituloCategoria}
-                                type='text'
-                                placeholder='Titulo de la categoría'
-                            ></input>
-                            <input
-                                onClick={handleSubmit}
-                                className='input-submit-crear-cat'
-                                type='image' 
-                                src={tituloCategoria==="" ? icono_anadir_disabled : icono_anadir_enabled} 
-                                disabled={tituloCategoria==="" ? true : false}
-                                alt='icono nueva categoría' 
-                                style={{width:'2em'}}>
-                            </input>
-                        </div>
-                    </form>
-                </>
-                :
-                <button className='boton-nueva-categoria' onClick={()=>{setMostrandoFormNuevaCategoria(true)}}>Crear nueva categoría</button>    
-            }
 
-            <p className='texto-click-volver'>Click en cualquier sitio para volver</p>
+                {
+                    mostrandoFormNuevaCategoria === true
+                        ?
+                        <>
+                            <form onSubmit={handleSubmit}>
+                                <div className="input-and-button">
+                                    <input
+                                        className='input-text-crear-cat'
+                                        onChange={handleChange}
+                                        value={tituloCategoria}
+                                        type='text'
+                                        placeholder='Titulo de la categoría'
+                                    ></input>
+                                    <input
+                                        onClick={handleSubmit}
+                                        className='input-submit-crear-cat'
+                                        type='image'
+                                        src={tituloCategoria === "" ? icono_anadir_disabled : icono_anadir_enabled}
+                                        disabled={tituloCategoria === "" ? true : false}
+                                        alt='icono nueva categoría'
+                                        style={{ width: '2em' }}>
+                                    </input>
+                                </div>
+                            </form>
+                        </>
+                        :
+                        <button className='boton-nueva-categoria' onClick={() => { setMostrandoFormNuevaCategoria(true) }}>Crear nueva categoría</button>
+                }
+
+                <p className='texto-click-volver'>Click en cualquier sitio para volver</p>
+            </div>
         </div>
     )
 }
