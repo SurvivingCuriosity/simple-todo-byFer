fbc31f830f9f2b0839b8b90c11d8106e3c123d6d
Fernando <ferrodest1999@gmail.com>
2023-01-20 13:25:26 +0100

    Fixed nav categorias

--- src/components/NavCategorias.js
+++ src/components/NavCategorias.js
@@ -1,37 +1,17 @@
 import React from "react";
-import icono_anadir_enabled from '../imgs/icono_anadir_enabled.svg'
-import { PantallaAjustes } from "./PantallaAjustes";
 
 export function NavCategorias(props) {
 
-    const [mostrandoFormNuevaCategoria, setMostrandoFormNuevaCategoria] = React.useState(false)
-
     const userClicksCategoria = (evt) => {
-        if (!evt.target.className === 'nav-categorias') {
-            props.callbackCategoriaActiva(evt.target.textContent);
+        if (evt.target.className === 'nav-categorias') {
+            return;
         }
+        props.callbackCategoriaActiva(evt.target.textContent);
 
-    }
-    const crearCategoria = (titulo) => {
-        props.callbackCrearCategoria(titulo);
-    }
-    const userBorraCategoria = (titulo) => {
-        props.callbackBorrarCategoria(titulo);
-    }
-    const userClosesForm = (evt) => {
-        setMostrandoFormNuevaCategoria(false);
     }
 
     return (
         <div className="nav-categorias-y-boton">
-            {mostrandoFormNuevaCategoria &&
-                <PantallaAjustes
-                    categorias={props.categorias}
-                    handleSubmitCallback={crearCategoria}
-                    handleCloseCallback={userClosesForm}
-                    handleBorrarCategoria={userBorraCategoria}
-                />}
-
             <div className='nav-categorias'>
                 <div className={`tab-header-categoria ${(props.categoriaActiva === '') ? `categoria-activa` : ``}`} onClick={userClicksCategoria}>Sin categoría</div>
                 {props.categorias.map((cat) => {
